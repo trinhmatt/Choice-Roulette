c348ca0867daf95729c6439adf6c9494a9c97d9f
Matthew  Trinh <matthewtrinh@Matthews-MacBook-Pro.local>
2017-07-16 16:20:21 -0400

    Disabled editing text display fields

--- decider.py
+++ decider.py
@@ -5,22 +5,30 @@ choices = []
 
 def add_choice():
     if entry.get() != '':
+        choices_display.config(state=NORMAL)
         choices.append(entry.get())
         choices_display.insert(END,entry.get()+', ')
         entry.delete(0, END)
+        choices_display.config(state=DISABLED)
 
 
 def choose():
     if choices[0] != '':
+        final_display.config(state=NORMAL)
         decision = random.randint(0,len(choices)-1)
         final_display.delete(1.0,END)
         final_display.insert(END, 'The decider has chosen ' + choices[decision] + '!')
+        final_display.config(state=DISABLED)
 
 def clear_choices():
     global choices
+    choices_display.config(state=NORMAL)  #To disable editing display boxes
+    final_display.config(state=NORMAL)
     choices = []
     choices_display.delete(1.0,END)
     final_display.delete(1.0,END)
+    choices_display.config(state=DISABLED) #To disable editing display boxes
+    final_display.config(state=DISABLED)
 
 
 root = Tk()
@@ -29,9 +37,13 @@ entry = Entry(root)
 add_button = Button(root, text = 'Add choice', command = add_choice)
 chooser_button = Button(root, text = 'Choose!', command = choose)
 clear_button = Button(root, text = 'Clear choices', command = clear_choices)
+
 choices_label = Label(root, text = "Choices")
+
 choices_display = Text(root, height = 10, width = 40)
+choices_display.config(state=DISABLED)
 final_display = Text(root, height = 5, width = 30)
+final_display.config(state=DISABLED)
 
 choices_label.grid(row = 0)
 choices_display.grid(row = 1)
