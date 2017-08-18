3fb0abe0c6d9f7c792fc3be2546b4251fd117819
Matthew  Trinh <matthewtrinh@Matthews-MacBook-Pro.local>
2017-08-18 17:00:02 -0400

    Each choice added in a new line, scrollable text box

--- decider.py
+++ decider.py
@@ -7,7 +7,7 @@ def add_choice():
     if entry.get() != '':
         choices_display.config(state=NORMAL)
         choices.append(entry.get())
-        choices_display.insert(END,entry.get()+', ')
+        choices_display.insert(END,entry.get()+'\n')
         entry.delete(0, END)
         choices_display.config(state=DISABLED)
 
@@ -38,12 +38,16 @@ add_button = Button(root, text = 'Add choice', command = add_choice)
 chooser_button = Button(root, text = 'Choose!', command = choose)
 clear_button = Button(root, text = 'Clear choices', command = clear_choices)
 
+
+
+
 choices_label = Label(root, text = "Choices")
 
 choices_display = Text(root, height = 10, width = 40)
 choices_display.config(state=DISABLED)
 final_display = Text(root, height = 5, width = 30)
 final_display.config(state=DISABLED)
+
 
 choices_label.grid(row = 0)
 choices_display.grid(row = 1)
@@ -52,6 +56,7 @@ entry.grid(row = 3)
 add_button.grid(row = 3,column = 1)
 chooser_button.grid(row = 3,column = 2)
 clear_button.grid(row = 4)
+
 
 root.title("Choice Lottery")
 root.mainloop()
